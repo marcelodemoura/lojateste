308ffe23ddb8107e8969298371b4e79a1df71e86
marcelo.cavalcanti <marcelodemoura14@gmail.com>
2023-06-08 17:56:51 -0300

    update

--- lojateste/src/main/java/com/br/marcelo/lojateste/dto/EmpresaDto.java
+++ lojateste/src/main/java/com/br/marcelo/lojateste/dto/EmpresaDto.java
@@ -1,4 +1,16 @@
 package com.br.marcelo.lojateste.dto;
 
+
+import com.br.marcelo.lojateste.entity.Empresa;
+
 public class EmpresaDto {
+    private String nome;
+    private String ativa;
+
+
+    public EmpresaDto(Empresa entity) {
+        this.nome = entity.getNome();
+        this.ativa = entity.getAtiva();
+
+    }
 }
