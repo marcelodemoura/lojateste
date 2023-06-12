394431fed457c16e28d6cf145ea734d1ac3b2308
marcelo.cavalcanti <marcelodemoura14@gmail.com>
2023-06-12 09:34:26 -0300

    "update"

--- lojateste/src/main/java/com/br/marcelo/lojateste/entity/Vendas.java
+++ lojateste/src/main/java/com/br/marcelo/lojateste/entity/Vendas.java
@@ -1,4 +1,16 @@
 package com.br.marcelo.lojateste.entity;
 
 public class Vendas {
+
+    private Long id;
+    private String TPdoces;
+    private String Qtde;
+    private String VTotal;
+    private String Cliente;
+    private String FPagamento;
+    private String DtCompra;
+    private String DtPagamento;
+    private String VPago;
+    private String  Pago;
+
 }
