cff0bfe12545e3a6534b813151e78326c9c3ec59
marcelo.cavalcanti <marcelodemoura14@gmail.com>
2023-06-12 10:35:34 -0300

    "update"

--- lojateste/src/main/java/com/br/marcelo/lojateste/entity/Vendas.java
+++ lojateste/src/main/java/com/br/marcelo/lojateste/entity/Vendas.java
@@ -1,16 +1,40 @@
 package com.br.marcelo.lojateste.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "/vendas")
 public class Vendas {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String TPdoces;
-    private String Qtde;
-    private String VTotal;
-    private String Cliente;
-    private String FPagamento;
-    private String DtCompra;
-    private String DtPagamento;
-    private String VPago;
-    private String  Pago;
+    @Column(name = "TP_Doces",nullable = false, length = 5)
+    private String tPdoces;
+    @Column(name = "Qtde",nullable = false, length = 5)
+    private String qtde;
+    @Column(name = "QTotal",nullable = false, length = 5)
+    private String qTotal;
+    @Column(name = "Cliente",nullable = false, length = 30)
+    private String cliente;
+    @Column(name = "FPagamento",nullable = false, length = 10)
+    private String fPagamento;
+    @Column(name = "DTCompra",nullable = false, length = 5)
+    private String dtCompra;
+    @Column(name = "DTPagamento",nullable = false, length = 5)
+    private String dtPagamento;
+    @Column(name = "VPago",nullable = false, length = 5)
+    private String vPago;
+    @Column(name = "Pago",nullable = false, length = 5)
+    private boolean Pago = false;
 
 }
